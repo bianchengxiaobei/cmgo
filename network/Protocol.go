43adb2ff3166b72758bf5032554fe405c37edfa4
bianchengxiaobei <443850741@qq.com>
2018-06-21 10:04:07 +0800

    修改protocol方法名字

--- network/Protocol.go
+++ network/Protocol.go
@@ -1,6 +1,6 @@
 package network
 
 type Protocol interface {
-	Encode(SocketSessionInterface,[]byte)(interface{},int,error)
-	Decode(SocketConnectInterface,interface{}) error
+	Decode(SocketSessionInterface,[]byte)(interface{},int,error)
+	Encode(SocketConnectInterface,interface{}) error
 }
